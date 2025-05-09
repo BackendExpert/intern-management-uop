ca4180536a17082904d0dfe142fa9d83f15952a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-09 06:55:03 +0530

    Create authController.js

---
+++ server/controller/authController.js
@@ -0,0 +1,14 @@
+const authController = {
+    signUp: async(req, res) => {
+        try{
+            const {
+                
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+};
+
+module.exports = authController;
