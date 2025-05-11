794092dc0dd48a4f12bbcf2bf56e384486e286b6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-11 11:49:01 +0530

    Update DashHome.jsx

--- client/src/pages/Dashboard/DashHome.jsx
+++ client/src/pages/Dashboard/DashHome.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import useRoleGuard from '../../hooks/useRoleGuard'
+import secureLocalStorage from 'react-secure-storage'
 
 const DashHome = () => {
+    const roleuser = secureLocalStorage.getItem('loginR')
     const isAllowed = useRoleGuard(['intern', 'staff', 'security', 'admin', 'director'])
     if (!isAllowed) return null
 
-    return (
-        <div>DashHome</div>
-    )
+    if (roleuser === 'director') {
+        return (
+            <div>director</div>
+        )
+    }
+    else if (roleuser === 'admin') {
+        return (
+            <div>admin</div>
+        )
+    }
+    else if (roleuser === 'security') {
+        return (
+            <div>security</div>
+        )
+    }
+    else if (roleuser === 'staff') {
+        return (
+            <div>staff</div>
+        )
+    }
+    else if (roleuser === 'intern') {
+        return (
+            <div>intern</div>
+        )
+    }
 }
 
 export default DashHome
