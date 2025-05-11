0bc5bca411258daba7fe42abe3a1071651caf32b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-11 11:18:26 +0530

    Update

--- client/src/pages/Dashboard/DashHome.jsx
+++ client/src/pages/Dashboard/DashHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const DashHome = () => {
-    
-  return (
-    <div>DashHome</div>
-  )
+    const isAllowed = useRoleGuard(['intern', 'staff', 'security', 'admin', 'director'])
+    if (!isAllowed) return null
+
+    return (
+        <div>DashHome</div>
+    )
 }
 
 export default DashHome
--- server/model/User.js
+++ server/model/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: "staff", enum: ['intern', 'staff', 'admin', 'director'] },
+    role: { type: String, default: "staff", enum: ['intern', 'staff', 'security', 'admin', 'director'] },
     isEmailVerify: { type: Boolean, default: false },
     isActive: { type: Boolean, default: false }
 }, {timestamps: true });
